e0b28c3407ee0a6362a8cc8c18e420b647c1e18d
Rajkumar Murugesan <90443069+rajm688@users.noreply.github.com>
2021-12-10 16:17:49 +0530

    Add files via upload

---
+++ DOMpractice/script.js
@@ -0,0 +1,27 @@
+// var a = document.getElementById("item2")
+// console.log(a)
+// a.innerHTML="hello2"
+// a.textContent="hello3"
+// a.innerText="final"
+// var b = document.getElementsByClassName("list-group-item")
+// b[0].textContent="raaj"
+// b[1].innerText="sudhan"
+// var c = document.getElementsByTagName("h3");
+// c.innerHTML="alternate"//not working
+// console.log(c)//not working
+// var d = document.querySelectorAll(".list-group-item")
+// console.log(d)
+// for(let i=0; i<d.length; i++){
+//     d[i].style.backgroundColor="#808080"
+// }
+// var newdiv = document.createElement("div");
+// newdiv.className="border-danger"
+// newdiv.id="newdiv"
+// var textnode = document.createTextNode("Hello World")
+// newdiv.appendChild(textnode)
+// var div = document.getElementById("class")
+// newdiv.insertBefore("div")
+let d = document.getElementsById(" ")
+function btnclick(){
+    console.log("hello")
+}
